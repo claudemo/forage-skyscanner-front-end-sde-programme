df28dc6d180097c1d8985afe0ad31d96f448ab97
claudemo <myliu.pro@gmail.com>
2023-09-04 09:21:22 -0400

    download necessory stuffs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,14 +13,14 @@ const App = () => (
   <div className={getClassName('App')}>
     <header className={getClassName('App__header')}>
       <div className={getClassName('App__header-inner')}>
-        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>Welcome to React + Backpack</BpkText>
+        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>Flight Schedule</BpkText>
       </div>
     </header>
     <main className={getClassName('App__main')}>
       <BpkText tagName="p" className={getClassName('App__text')}>
         To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
       </BpkText>
-      <BpkButton onClick={() => alert('It works!')}>Click me</BpkButton>
+      <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
     </main>
   </div>
 );
